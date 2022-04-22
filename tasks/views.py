259c5cafb48610db165d5e9f77ffee620301fd08
Mithilesh Joshi <mithileshjoshi@Mithileshs-MacBook-Air.local>
2022-04-22 09:45:22 +0530

    session function added to task

--- tasks/views.py
+++ tasks/views.py
@@ -6,11 +6,13 @@ from django.urls import reverse
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task ")
 
-tasks = ['foo','bar','buz']
+
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, 'tasks/index.html', {
-        "tasks":tasks
+        "tasks":request.session["tasks"]
     })
 
 def add(request):
@@ -18,7 +20,7 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
